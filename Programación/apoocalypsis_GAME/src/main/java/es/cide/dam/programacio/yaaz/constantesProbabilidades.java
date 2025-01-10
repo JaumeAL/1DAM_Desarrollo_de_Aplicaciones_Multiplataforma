d3985fe7ed1a248d0fa100573ee8cf81ccde0912
«JaumeAI» <jsalas2@nigul.cide.es>
2025-01-10 17:18:44 +0100

    Refactor probabilidades de aparición de artefactos en novaCiutat y agregar clase constantesProbabilidades

---
+++ Programación/apoocalypsis_GAME/src/main/java/es/cide/dam/programacio/yaaz/constantesProbabilidades.java
@@ -0,0 +1,10 @@
+package es.cide.dam.programacio.yaaz;
+
+public class constantesProbabilidades {
+    //Probabilidades de aparición de los artefactos en novaCiutat
+    public static final int armaDeFoc = 1;
+    public static final int armaDAcer = 4;
+    public static final int FARMACIOLA = 9;
+    public static final int ESCUD = 19;
+
+}
--- Programación/apoocalypsis_GAME/src/main/java/es/cide/dam/programacio/yaaz/novaCiutat.java
+++ Programación/apoocalypsis_GAME/src/main/java/es/cide/dam/programacio/yaaz/novaCiutat.java
@@ -24,17 +24,17 @@ public class novaCiutat extends ciutat {
         for (int i = 0; i < artefactes.length; i++) { //bucle para recorrer el array de artefactos
             int probabilidad = rand.nextInt(100) + 1; // Generamos un número aleatorio entre 1 y 100
 
-            if (!armaDeFocPosicionada && probabilidad <= 1) { // Si no se ha posicionado un arma y la probabilidad es menor o igual a 1 (1%)
-                artefactes[i] = new armaDeFoc();              // Creamos un nuevo artefacto de tipo armaDeFoc
-                armaDeFocPosicionada = true;                  // Marcamos que ya se ha posicionado un arma ya que solo puede haber una
-            } else if (probabilidad <= 4) {                   // Si la probabilidad es menor o igual a 4 (4%), pero mayor que 1 (3% adicional)
-                artefactes[i] = new armaDAcer();              // Creamos un nuevo artefacto de tipo armaDAcer
-            } else if (probabilidad <= 9) {                   // Si la probabilidad es menor o igual a 9 (9%), pero mayor que 4 (5% adicional)
-                artefactes[i] = new farmaciola();             // Creamos un nuevo artefacto de tipo farmaciola
-            } else if (probabilidad <= 19) {                  // Si la probabilidad es menor o igual a 19 (19%), pero mayor que 9 (10% adicional)
-                artefactes[i] = new escud();                  // Creamos un nuevo artefacto de tipo escud
-            } else {                                          // Si no se cumple ninguna de las condiciones anteriores
-                artefactes[i] = null;                         // No hay artefacto en esta posición
+            if (!armaDeFocPosicionada && probabilidad <= constantesProbabilidades.armaDeFoc) { // Si no se ha posicionado un arma y la probabilidad es menor o igual a 1 (1%)
+                artefactes[i] = new armaDeFoc();                              // Creamos un nuevo artefacto de tipo armaDeFoc
+                armaDeFocPosicionada = true;                                  // Marcamos que ya se ha posicionado un arma ya que solo puede haber una
+            } else if (probabilidad <= constantesProbabilidades.armaDAcer) {  // Si la probabilidad es menor o igual a 4 
+                artefactes[i] = new armaDAcer();                              // Creamos un nuevo artefacto de tipo armaDAcer
+            } else if (probabilidad <= constantesProbabilidades.FARMACIOLA) { // Si la probabilidad es menor o igual a 9 
+                artefactes[i] = new farmaciola();                             // Creamos un nuevo artefacto de tipo farmaciola
+            } else if (probabilidad <= constantesProbabilidades.ESCUD) {      // Si la probabilidad es menor o igual a 19
+                artefactes[i] = new escud();                                  // Creamos un nuevo artefacto de tipo escud
+            } else {                                                          // Si no se cumple ninguna de las condiciones anteriores
+                artefactes[i] = null;                                         // No hay artefacto en esta posición
             }
         }
     }
