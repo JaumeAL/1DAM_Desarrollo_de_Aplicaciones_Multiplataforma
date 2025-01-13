aaa9ef1246dc75a9cfb8a7bb2fa8c51a8747b1a1
«JaumeAI» <jsalas2@nigul.cide.es>
2025-01-13 16:00:11 +0100

    archivos de prueba HTML, CSS y JavaScript para la interfaz

---
+++ Lenguaje de marcas/pruebasJS/script/scripts.js
@@ -0,0 +1,4 @@
+const parrafo = document.getElementById('parrafo');
+parrafo.textContent = 'Parrafo modificado desde el archivo js'; 
+
+console.log("Hola mundo desde el archivo scripts.js"); // Esto se muestra en la consola del navegador
