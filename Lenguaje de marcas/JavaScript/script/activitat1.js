238894256745baada5c30d994b632d131ad5ed68
«JaumeAI» <jsalas2@nigul.cide.es>
2025-01-14 16:43:25 +0100

    calcular la media de 3 examenes

--- Lenguaje de marcas/JavaScript/script/activitat1.js
+++ Lenguaje de marcas/JavaScript/script/activitat1.js
@@ -1,6 +1,9 @@
-var nota1 = prompt("Introduce la nota del primer examen: ");
-var nota2 = prompt("Introduce la segunda nota: ");
-var nota3 = prompt("Introduce la tercera nota: ");
+//calcular la media de 3 examenes
+var nota1 = parseFloat(prompt("Introduce la nota del primer examen: "));
+var nota2 = parseFloat(prompt("Introduce la segunda nota: "));
+var nota3 = parseFloat(prompt("Introduce la tercera nota: "));
 
-var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
+var media = (nota1 + nota2 + nota3) / 3;
 alert("La media de las tres notas es: " + media);
+
+
