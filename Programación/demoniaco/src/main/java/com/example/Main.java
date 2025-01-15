ad03ab669312492bc6c05b5bb23c5ae94ed3ffe4
JaumeAI <jsalas2@nigul.cide.es>
2025-01-15 13:30:04 +0100

    Corregir la clase Cotxe y añadir la clase Moto con métodos para conduir y albergar persones.

--- Programación/demoniaco/src/main/java/com/example/Main.java
+++ Programación/demoniaco/src/main/java/com/example/Main.java
@@ -21,7 +21,7 @@ abstract class Vehicle {
     }
 }
 //Classe Cotxe
-class Cotxe extends vehicle implements interficieConduir,interficieHabitabilitat {
+class Cotxe extends Vehicle implements interficieConduir,interficieHabitabilitat {
     private int capacitat;
     
     public Cotxe(String nom, int capacitat) {
@@ -36,7 +36,7 @@ class Cotxe extends vehicle implements interficieConduir,interficieHabitabilitat
 
     @Override
     public int obtenirVelocitatMaxima() {
-        return 200; //velocitat màxima de 200 km/h
+        return 180; //velocitat màxima de 180 km/h
     }
 
     @Override 
@@ -54,13 +54,53 @@ class Cotxe extends vehicle implements interficieConduir,interficieHabitabilitat
     }
 }
 
+//Classe Moto
+class Moto extends Vehicle implements interficieConduir,interficieHabitabilitat {
+    private int capacitat;
+    
+    public Moto(String nom, int capacitat) {
+        super(nom);
+        this.capacitat = capacitat >= 2 ? capacitat : 2; //com a mínim 4 places
+
+    }
+
+    @Override
+    public void conduir() {
+        System.out.println("Estic conduint la moto " + nom + ".");
+    }
+
+    @Override
+    public int obtenirVelocitatMaxima() {
+        return 200; //velocitat màxima de 150 km/h
+    }
+
+    @Override
+    public void albergarPersones(int nombre) {
+        if (nombre <= capacitat) {
+            System.out.println("La moto " + nom + " alberga " + nombre + " persones.");
+        } else {
+            System.out.println("La moto " + nom + " no pot albergar més de " + capacitat + " persones.");
+        }
+    }
+
+    @Override
+    public int obtenirCapacitat() {
+        return capacitat;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        cotxe miCotxe = new Cotxe("Ferrari F40", 5);
+        //Cream un cotxe
+        Cotxe miCotxe = new Cotxe("Ferrari F40", 5);
         miCotxe.descriure();
         miCotxe.albergarPersones(4);
+
+        //Cream una moto
+        Moto miMoto = new Moto("Yamaha R1",2);
+        miMoto.descriure();
+        miMoto.albergarPersones(2);
 
     }
 }
