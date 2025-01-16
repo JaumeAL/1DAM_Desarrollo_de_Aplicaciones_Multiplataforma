9bae782c48182e31e3bce705fdc7e7d955fc1d7d
«JaumeAI» <jsalas2@nigul.cide.es>
2025-01-16 17:09:30 +0100

    Suspendido,Aprobado,Notable, Sobresaliente

--- Lenguaje de marcas/JavaScript/script/activitat1.js
+++ Lenguaje de marcas/JavaScript/script/activitat1.js
@@ -6,5 +6,20 @@ var nota3 = parseFloat(prompt("Introduce la tercera nota: "));
 var media = (nota1 + nota2 + nota3) / 3;
 alert("La media de las tres notas es: " + media); //por alerta
 console.log("La media de las tres notas es: " + media); // por consola
+
 const parrafo = document.getElementById('parrafo');
 parrafo.textContent = media; // por pantalla
+
+if (media < 5) {
+    parrafo.textContent = media + " Suspendido";
+    console.log("Suspendido");
+} else if (media >= 5 && media < 7) {
+    parrafo.textContent = media + " Aprobado";
+    console.log("Aprobado");
+} else if (media >= 7 && media < 9) {
+    parrafo.textContent = media + " Notable";
+    console.log("Notable");
+} else {
+    parrafo.textContent = media + " Sobresaliente";
+    console.log("Sobresaliente");
+}
