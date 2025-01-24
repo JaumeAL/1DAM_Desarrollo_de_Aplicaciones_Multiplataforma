fa232fa3ec1db58c57082c15f48278a6065d1006
JaumeAI <jsalas2@nigul.cide.es>
2025-01-24 09:45:37 +0100

    Agregar interficies y la superclase animal

--- Programación/practi_exam_24_01_25/src/main/java/com/example/Main.java
+++ Programación/practi_exam_24_01_25/src/main/java/com/example/Main.java
@@ -1,5 +1,36 @@
+/* {
+    "name": "Jaume Albert",
+    "surnames": "Salas Pastor",
+    "age": "18"
+    }
+ */
 package com.example;
+//Interficie Terrestre
+interface InnerMain {
+    void caminar();
+} 
+//Interficie Terrestre
+interface Volador {
+    void volar();
+} 
+//Interficie Terrestre
+interface Aquatic {
+    void nedar();
+} 
+//Clase abstracta Animal
+abstract class Animal{
+    protected String nom;
+    protected String tipus;
 
+    public Animal(String nom, String tipus){
+        this.nom = nom;
+        this.tipus = tipus;
+    }
+    public String getTipus() {
+        return tipus;
+    }
+    
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
