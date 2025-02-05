3bf106c80a3e9ca4d3d9bb9c45b83a7a5d59fe99
JaumeAL <jsalas2@nigul.cide.es>
2025-02-05 17:14:06 +0100

    OK

--- Lenguaje de marcas/JavaScript/javaScript/canvioEstilos.js
+++ Lenguaje de marcas/JavaScript/javaScript/canvioEstilos.js
@@ -5,17 +5,26 @@ function lletraGran() {
     let tamanio = parseInt(estilo) + 1;
     parrafo.style.fontSize = tamanio + "px";
 }
+//Boton disminuir letra
 function lletraPetita() {
     let parrafo = document.getElementById("parrafo");
     let estilo = window.getComputedStyle(parrafo, null).getPropertyValue("font-size");
     let tamanio = parseInt(estilo) - 1;
     parrafo.style.fontSize = tamanio + "px";
 }
-
 // Boton cambiar color
 function canviarColor() {
     let parrafo = document.getElementById("parrafo");
     let colores = ["#FF5733", "#33FF57", "#3357FF", "#F3FF33", "#FF33F3"];
     let color = colores[Math.floor(Math.random() * colores.length)];
     parrafo.style.color = color;
+}
+
+//Boton añadir texto
+function afegirParagraf() {
+    var text = prompt("Introduce un texto");
+    let nuevo = document.createElement("p");
+    nuevo.innerHTML = text;
+    nuevo.appendChild(text);
+    document.body.appendChild(nuevo);
 }
