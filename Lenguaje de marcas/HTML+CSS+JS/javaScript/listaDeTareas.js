a265b5d542950fb04785a14d27c1770c7c80343c
JaumeAL <jsalas2@nigul.cide.es>
2025-02-24 16:22:46 +0100

    Agregar funcionalidad para eliminar tareas completadas y mejorar la interfaz de usuario con estilos para tareas marcadas.

--- Lenguaje de marcas/HTML+CSS+JS/javaScript/listaDeTareas.js
+++ Lenguaje de marcas/HTML+CSS+JS/javaScript/listaDeTareas.js
@@ -1,8 +1,12 @@
+/* Afegir tasques: Els usuaris han de poder introduir tasques mitjançant un camp de text i un botó per afegir-les a la llista. Cada tasca afegida ha d'aparèixer a la llista de tasques.
+Marcar tasques com a completades: Cada tasca a la llista hauria de tenir un checkbox o algun altre mecanisme per marcar-la com a completada. Quan una tasca està marcada com a completada, ha de mostrar-se de manera diferent (p. ex., ratllada o amb un color diferent).
+Eliminar tasques: Ha d'haver-hi una opció per eliminar tasques individuals de la llista. També una funció per eliminar totes les tasques completades amb un sol clic.
+Filtrar tasques: Creeu botons per filtrar les tasques mostrades: totes, actives (no completades), o completades. */
 function agregar() { 
     var tarea = document.getElementById("tarea").value;
     var lista = document.getElementById("lista");
     var nuevaTarea = document.createElement("li");
-    
+
     nuevaTarea.textContent = tarea;
     lista.appendChild(nuevaTarea);
     
@@ -12,8 +16,10 @@ function agregar() {
     check.onclick = function() {
         if (check.checked) {
             nuevaTarea.style.textDecoration = "line-through";
+            nuevaTarea.style.color = "gray";
         } else {
             nuevaTarea.style.textDecoration = "none";
+            nuevaTarea.style.color = "black";
         }
     }
     
@@ -26,4 +32,15 @@ function agregar() {
     }
 
 
+}
+//BOTON PARA ELIMINAR TAREAS COMPLETADAS
+function eliminarCompletadas() {
+    var lista = document.getElementById("lista");
+    var tareas = lista.children;
+    for (var i = 0; i < tareas.length; i++) {
+        if (tareas[i].children[0].checked) {
+            lista.removeChild(tareas[i]);
+            i--;
+        }
+    }
 }
