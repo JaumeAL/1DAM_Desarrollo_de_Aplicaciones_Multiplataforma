2e5047563d221831790832a4d895d0179eba0a60
JaumeAL <jsalas2@nigul.cide.es>
2025-03-07 18:54:30 +0100

    Actualizar estilos y funcionalidad del formulario de comentarios

--- Lenguaje de marcas/HTML+CSS+JS/javaScript/examenJaume.js
+++ Lenguaje de marcas/HTML+CSS+JS/javaScript/examenJaume.js
@@ -1,6 +1,5 @@
-
+//Boton Contar
 var contador = 0;
-const final = 10;
 function Contar() {
     if (contador < 10) {
         contador ++;
@@ -13,10 +12,38 @@ function Contar() {
     
 }
 
+//Boton Reiniciar contador
 function Reiniciar() {
     document.getElementById("parrafo").textContent = "0";
     contador = 0;
     document.getElementById("reiniciar").style.display ="none";
     document.getElementById("contar").style.display ="flex";    
 
+}
+// Para manejar el evento submit del formulario y que no se resetee la página
+document.getElementById("formComentarios").addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+
+// Boton añadir comentario
+function Añadir(){
+    var inputComentario = document.getElementById("comentario").value;
+    if (inputComentario === "") {
+        alert("Campo vacio :(")
+    }else{
+    var lista = document.getElementById("lista")
+    var nuevoComentario = document.createElement("li");
+    var textoComentario = document.createElement("span");
+
+    textoComentario.textContent=inputComentario;
+    nuevoComentario.appendChild(textoComentario);
+
+    lista.appendChild(nuevoComentario);
+    inputComentario = document.getElementById("comentario").value = "";
+    }
+}
+// Boton Eliminar comentario
+function Eliminar(){
+    var lista = document.getElementById("lista");
+    lista.removeChild(lista.lastChild);
 }
